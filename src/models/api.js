30f183fd717a0f25dd0ae060d7450ea35f310c8d
shashank9927 <shashankat99@gmail.com>
2025-06-01 22:36:17 +0200

    implement api-rate-limiter schema

---
+++ src/models/api.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const apiKeySchema = new mongoose.Schema({
+    key:{
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+    },
+    userId:{
+        type: String,
+        required: true,
+    },
+    limits: {
+        requestPerMinute: {
+            type: Number,
+            required: true,
+            default: 60
+        }
+    },
+    windowStart:{
+        type: Date,
+        default: Date.now
+    },
+    requestCount: {
+        type: Number,
+        default: 0,
+    },
+    excessiveRequestCount: {
+        type: Number,
+        default: 0
+    },
+    isBlackListed: {
+        type: Boolean,
+        default: false
+    },
+    blackListedUntil:{
+        type: Date,
+        default: null
+    },
+
+    },
+    {
+        timestamps:true        
+    }
+);
+
+const rateLimitModel = mongoose.model('ApiKey',apiKeySchema)
+
+module.exports = {rateLimitModel}
