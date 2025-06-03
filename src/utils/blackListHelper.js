b49a5c34dc6b62ca400751352a41d1530197e00b
shashank9927 <shashankat99@gmail.com>
2025-06-03 21:36:33 +0200

    Implement black list helper function

---
+++ src/utils/blackListHelper.js
@@ -0,0 +1,52 @@
+const rateLimitModel = require('../models/api.js');
+
+// check if an API is blacklisted and auto fix if expired
+
+const checkAndFixBlacklist = async (apiKey) => {
+    try{
+        //fetch API Key document
+        const apiKeyDoc = await rateLimitModel.findOne({key: apiKey});
+        if(!apiKeyDoc){
+            throw new Error('Invalid API Key');
+        }
+    
+        const now = new Date();
+
+        //check if the API Key is blacklisted
+        const isBlacklisted = apiKeyDoc.isBlacklisted && apiKeyDoc.blacklistedUntil && apiKeyDoc.blacklistedUntil > now;
+
+        //if marked blacklisted but date has expired, fix it immediately
+        if(apiKeyDoc.isBlacklisted && (!apiKeyDoc.blacklistedUntil || apiKeyDoc.blacklistedUntil <= now)){
+            console.log(`Auto fixing expired blacklist for the key: ${apiKey} `)
+
+            await rateLimitModel.updateOne(
+                {
+                    key: apiKey
+                },
+                {
+                    $set: {
+                        isBlacklisted: false,
+                        blacklistedUntil: null,
+                        excessiveRequestCount: 0
+                    }
+                }
+            );
+
+            //update the document in database to reflect changes
+
+            apiKeyDoc.isBlacklisted = false;
+            apiKeyDoc.blacklistedUntil = null;
+            apiKeyDoc.excessiveRequestCount = 0;
+
+        }
+
+        return { isBlacklisted, apiKeyDoc};
+
+    } 
+    catch(error){
+        console.error('Error checking blacklist status: ',error);
+        throw error;
+    }
+};
+
+module.exports = {checkAndFixBlacklist};
